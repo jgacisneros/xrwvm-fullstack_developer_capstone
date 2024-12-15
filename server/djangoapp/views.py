2ad5128fd8bdddf509ee9dd9eaef29e51097cdee
jgacisneros <51058197+jgacisneros@users.noreply.github.com>
2024-12-15 13:22:46 -0800

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -128,7 +128,7 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         # data = json.loads(request.body)
         try:
             # response = post_review(data)
