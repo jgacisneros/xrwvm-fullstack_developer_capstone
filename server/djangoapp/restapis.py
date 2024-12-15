217a1b3fa59dee4b39c6b2715fd121b9be697f43
jgacisneros <51058197+jgacisneros@users.noreply.github.com>
2024-12-15 09:02:17 -0800

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -26,9 +26,8 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
-        # If any error occurs
-        print("Network exception occurred")
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
 
 
 # def analyze_review_sentiments(text):
